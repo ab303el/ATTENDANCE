11055b22cd09d013c18c0775965f22a3ad3f101c
abel demelash <abel303.ad@gmail.com>
2026-05-12 04:36:59 +0300

    update _

--- attendance/settings.py
+++ attendance/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
 
 # This is the "Railway Standard" way
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.parse('postgresql://postgres.wcdnwwnhonvfdppfzgeg:HtvDDaVK9zP3rst5@://supabase.com')
 }
 
 # Password validation
