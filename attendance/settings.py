96a1c650f66abfdbc9a1ba5b6f523ddeafd6dde1
abel demelash <abel303.ad@gmail.com>
2026-05-12 06:09:03 +0300

    update _

--- attendance/settings.py
+++ attendance/settings.py
@@ -112,9 +112,13 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # This is the "Railway Standard" way
 
 # Replace your current block with this:
+import dj_database_url
+
+# This MUST be the only DATABASES block in the file
 DATABASES = {
     'default': dj_database_url.parse('postgresql://postgres.wcdnwwnhonvfdppfzgeg:HtvDDaVK9zP3rst5@://supabase.com')
 }
+
 
 
 
